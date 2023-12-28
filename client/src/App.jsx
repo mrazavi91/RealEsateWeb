a74f6a6ab121ad86457a7afc7740e9722ee642b0
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2023-12-28 16:59:47 +0000

    Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import Signin from "./pages/Signin"
+import Profile from "./pages/Profile"
+import Signup from "./pages/Signup"
+
 
 export default function App() {
   return (
-    <div className='text-rose-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<Signup />}/>
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
