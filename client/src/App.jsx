ee3efd8453aea09c7f8efbf7830fc22afb789d43
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2023-12-28 17:39:16 +0000

    Header added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import Home from "./pages/Home"
 import Signin from "./pages/Signin"
 import Profile from "./pages/Profile"
 import Signup from "./pages/Signup"
+import Header from './components/Header'
 
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/sign-in' element={<Signin />} />
