a7d3f3c93e975ed35b2ae9e06274d55fb1a78ded
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2023-12-29 12:20:46 +0000

    Sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js"
+import jwt from "jsonwebtoken"
 
 
 export const signup = async (req,res, next) => {
@@ -16,6 +17,33 @@ export const signup = async (req,res, next) => {
         // handling the error using the middle ware and next function
         next(error)
         
+    }
+
+}
+
+export const signin = async (req, res, next) => {
+
+    const { email, password } = req.body
+
+    try {
+        const validUser = await User.findOne({ email })
+        if (!validUser) return next(errorHandler(404, "User Not Found"))
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) return next(errorHandler(401, "Invalid Password"))
+
+        // hashing for authentication
+        const token = jwt.sign({ id: validUser.id }, process.env.JWT_SECRET)
+
+        // removing password using the _.doc
+        const { password: pass, ...rest } = validUser._doc
+        
+        // save in the cookie the token 
+        res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest)
+
+    } catch (error) {
+        // handling the error using the middle ware and next function
+        next(error)
+
     }
 
 }
