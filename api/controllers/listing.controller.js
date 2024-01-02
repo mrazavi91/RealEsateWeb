ed7ea7abb7c9b06f73d9d020e355134d455117b1
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2024-01-02 18:55:06 +0000

    api update listing

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js"
+import { errorHandler } from "../utils/error.js";
 
 
 
@@ -27,11 +28,42 @@ export const deleteListing = async (req, res, next) => {
     }
 
     try {
+
         await Listing.findByIdAndDelete(req.params.id)  
         res.status(200).json("listing has been deleted")
+
     } catch (error) {
         next(error)
     }
+}
+
+
+export const updateListing = async (req, res, next) => {
+    
+
+    try {
+        const listing = await Listing.findById(req.params.id)
+
+        if (!listing) {
+            return next(errorHandler(404, "Listing not found"))
+        }
+
+        if (req.user.id !== listing.userRef) {
+            return next(errorHandler(401, "You can only edit your own listings"))
+        }
+
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        )
+
+        res.status(200).json(updatedListing)
+        
+    } catch (error) {
+        next(error)
+    }
+    
 }
 
 
