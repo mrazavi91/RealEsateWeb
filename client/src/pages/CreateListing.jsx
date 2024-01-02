2d342ce05f979c74544e2ecac59fb663cafd9918
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2024-01-02 23:14:37 +0000

    UI Listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -288,7 +288,9 @@ export default function CreateListing() {
                           />
                           <div className='flex flex-col items-center'>
                               <p>Regular price</p>
-                              <span className='text-xs'>($ / month)</span>
+                                {formData.type === 'rent' && (
+                                    <span className='text-xs'>($ / month)</span>
+                                )}
                           </div>
                           
                       </div>
@@ -305,7 +307,10 @@ export default function CreateListing() {
                           />
                           <div className='flex flex-col items-center'>
                               <p>Discounted price</p>
-                              <span className='text-xs'>($ / month)</span>
+                              {formData.type === 'rent' && (
+                                  <span className='text-xs'>($ / month)</span>
+                              )}
+
                           </div>
                       </div>)}
 
