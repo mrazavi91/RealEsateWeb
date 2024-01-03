36c9cbdc764d9db1bf0275eade5e17e44a7160b6
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2024-01-03 16:47:40 +0000

    update header search bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaSearch } from "react-icons/fa"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 export default function Header() {
-  const { currentUser } = useSelector(state => state.user)
+    const { currentUser } = useSelector(state => state.user)
+    const [searchTerm, setSearchTerm] = useState('')
+    const navigate = useNavigate()
+
+    const submitHandler = async (e) => {
+        e.preventDefault()
+        const urlParams = new URLSearchParams(window.location.search)
+        urlParams.set('searchTerm' , searchTerm)
+        const searchQuery = urlParams.toString()
+        navigate(`/search?${searchQuery}`)
+    }
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search)
+        const searchTermFromUrl = urlParams.get('searchTerm')
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl)
+        }
+        
+    },[location.search])
     
   return (
       <header className='bg-slate-200 shadow-md'>
@@ -16,10 +35,20 @@ export default function Header() {
                   </h1>
               </Link>  
               
-              <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                  <input type='text' placeholder="Search..."
-                      className='bg-transparent focus:outline-none w-24 sm:w-64' />
-                  <FaSearch className='text-slate-600'/>
+              <form
+                  onSubmit={submitHandler }
+                  className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                  <input
+                      onChange={(e) => setSearchTerm(e.target.value)}
+                      value={searchTerm}
+                      type='text'
+                      placeholder="Search..."
+                      className='bg-transparent focus:outline-none w-24 sm:w-64'
+                  />
+                  <button>
+                      <FaSearch className='text-slate-600' />
+                  </button>
+                  
               </form>
 
               <ul className='flex gap-4'>
