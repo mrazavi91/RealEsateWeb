fdaa436930673b4ea9f41005e20fd7f2a3e9a903
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2024-01-04 12:37:18 +0000

    About Page completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,37 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-4xl font-bold mb-4 text-slate-800'>About FindMyHome</h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Cursus in hac habitasse platea dictumst. Suscipit tellus mauris a diam maecenas sed.
+        Nunc consequat interdum varius sit amet mattis vulputate enim. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in.
+        Tellus integer feugiat scelerisque varius. Pharetra et ultrices neque ornare aenean euismod. Netus et malesuada fames ac turpis egestas integer eget aliquet.
+        Nisl nunc mi ipsum faucibus. Interdum velit euismod in pellentesque massa placerat. Auctor eu augue ut lectus.
+        Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Ut etiam sit amet nisl.
+        Ut aliquam purus sit amet. Id cursus metus aliquam eleifend mi in.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Consequat ac felis donec et. Nisl tincidunt eget nullam non nisi est sit amet. Fringilla urna porttitor rhoncus dolor.
+        Neque volutpat ac tincidunt vitae semper quis lectus nulla. Amet cursus sit amet dictum sit amet. Feugiat vivamus at augue eget.
+        Eget mauris pharetra et ultrices neque ornare. Justo donec enim diam vulputate. Tellus at urna condimentum mattis pellentesque id nibh tortor id.
+        Nisl purus in mollis nunc sed id semper risus in. Vitae nunc sed velit dignissim sodales ut eu. Venenatis lectus magna fringilla urna.
+        Porttitor lacus luctus accumsan tortor posuere ac. Fermentum posuere urna nec tincidunt praesent semper feugiat. Fermentum dui faucibus in ornare quam.
+        Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien.
+
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Mauris ultrices eros in cursus turpis massa. Malesuada fames ac turpis egestas sed tempus. Pharetra diam sit amet nisl suscipit adipiscing bibendum.
+        Facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Accumsan tortor posuere ac ut consequat.
+        Purus gravida quis blandit turpis cursus in hac habitasse platea. Erat velit scelerisque in dictum non consectetur a erat nam. Blandit cursus risus at ultrices.
+        Tempus quam pellentesque nec nam. Sodales ut eu sem integer vitae justo eget magna. Vel turpis nunc eget lorem dolor sed. Semper quis lectus nulla at.
+        Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Egestas tellus rutrum tellus pellentesque.
+        Duis tristique sollicitudin nibh sit amet commodo.
+      </p>
+    </div>
   )
 }
